4a29fa61e4aceaead90e346a6fd0f04d83d593de
Martin Bukatovič <mbukatov@redhat.com>
2019-03-07 14:14:32 +0100

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-ansible-playbook',
-    version='0.3.0',
+    version='0.4.0',
     author='Martin Bukatovič',
     author_email='mbukatov@redhat.com',
     maintainer='Martin Bukatovič',
